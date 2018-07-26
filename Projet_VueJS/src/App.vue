e16a24544bdaf2e3072a7373f0e3bf7464c045f6
Carmelina NAPOLITANO <Carmelina NAPOLITANO@Digital-Grenoble.local>
2018-07-26 11:36:24 +0200

    Affichage des deux machines à café

---
+++ Projet_VueJS/src/App.vue
@@ -0,0 +1,83 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+    </ul>
+    <h2>Parc des machines à café</h2>
+    <li>
+      <ol>Nom : DeLonghi</ol>
+      <ol>Statut : En fonction</ol>
+      <ol>Date de création : 26.07.2018</ol>
+      <ul><img v-bind:src="link1"/></ul>
+    </li>
+    <li>
+      <ol>Nom : DeLonghi</ol>
+      <ol>Statut : En panne</ol>
+      <ol>Date de création : 26.07.2018</ol>
+      <ul><img v-bind:src="link2"/></ul>
+    </li>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      link1:'./src/assets/MachineAcafeEnFonction.jpg',
+      link2:'./src/assets/MachineAcafeEnPanne.jpg'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+ol {
+  text-align: left;
+  padding: 0; 
+}
+</style>
+
+
